48bcc295c3a040fad7c6fc2bf21c57b9fe701ff3
hwan0418 <hwan0418@kaist.ac.kr>
2023-05-28 17:14:52 +0900

    Integrate Img2Txt function in gpt.py

--- img2txt/img2txt.py
+++ img2txt/img2txt.py
@@ -3,11 +3,11 @@ import subprocess
 from PIL import Image
 from lavis.models import load_model_and_preprocess
 
-if __name__=='__main__':
-	path = input("Please pass me a file path of image : ")
+def img2txt(imgdir):
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	raw_image = Image.open(path).convert("RGB")
+	raw_image = Image.open(imgdir).convert("RGB")
 	model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="large_coco", is_eval=True, device=device)
 	image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
 	caption = model.generate({"image": image})[0]
-	print(caption)
+ 
+	return caption
